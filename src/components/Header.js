ecbf71959b4303fdb11b8f1ae2bf7bac1e92265d
sahanmndl <mondalsahan053@gmail.com>
2022-10-07 11:54:46 +0530

    Header creation

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Code } from "@mui/icons-material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+
+const Header = () => {
+    return (
+        <AppBar style={{ background: '#F8F8F8', paddingLeft: '15%', paddingRight: '15%' }} 
+            position="static" 
+            component="nav"
+            elevation={1}
+        >
+            <Toolbar disableGutters sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <Code sx={{ marginRight: 1, color: '#757575' }} />
+                    <Typography sx={{ fontWeight: 700, color: '#757575', textDecoration: 'none' }}
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                    >
+                        UPCODING
+                    </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <Button sx={{ color: '#757575', marginRight: 2, textTransform: 'none', fontSize: 16 }}>
+                        Ongoing
+                    </Button>
+                    <Button sx={{ color: '#757575', textTransform: 'none', fontSize: 16 }}>
+                        Upcoming
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
