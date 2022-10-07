f281e118179457e0b9ce1d731e6aea9f4dea0544
Bhumika Mange <bhumikamange13@gmail.com>
2022-10-07 18:48:37 +0530

    Sorting feature added

--- src/pages/Contests/Upcoming.js
+++ src/pages/Contests/Upcoming.js
@@ -19,6 +19,14 @@ import leetcodeLogo from '../../assets/leetcode.png';
 import topcoderLogo from '../../assets/topcoder.png';
 import placeholderLogo from '../../assets/placeholder.png';
 import './Contests.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
 import Colors from '../../utils/Colors';
 
 const Upcoming = () => {
@@ -27,12 +35,19 @@ const Upcoming = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [refresh, setRefresh] = useState(false);
+  const [temp, setTemp] = useState([])
+  var options = ["ALL"];
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   const fetchAllContests = async () => {
     try {
       const response = await fetch(`https://kontests.net/api/v1/all`);
       const json = await response.json();
       setContests([...json]);
+      setTemp([...json])
     } catch (e) {
       console.log(e);
       setError(e);
@@ -42,9 +57,45 @@ const Upcoming = () => {
     }
   };
 
+  contests.map((i) => {
+    return options.push(i.site);
+  })
+  options = options.filter((v, i, a) => a.indexOf(v) === i);
+  console.log(options);
   const onRefresh = () => {
     setRefresh(true);
     fetchAllContests();
+  };
+
+
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (event, index, option) => {
+    setSelectedIndex(index);
+    console.log(option);
+    setOpen(false);
+    if (option != "ALL") {
+      setTemp(contests.filter((i) => {
+        console.log(i.site,option);
+        return i.site == option;
+      }))
+    }
+  };
+
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
 
   const keyGenerator = () =>
@@ -64,7 +115,56 @@ const Upcoming = () => {
                 Name
               </TableCell>
               <TableCell align="right" style={{ fontWeight: '700' }}>
-                Site
+                Site :  <ButtonGroup variant="" color='info' ref={anchorRef} aria-label="split button">
+                  {/* <Button size='small' onClick={handleClick}>{options[selectedIndex]}</Button> */}
+                  <Button
+                    size="small"
+                    aria-controls={open ? 'split-button-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-label="select merge strategy"
+                    aria-haspopup="menu"
+                    onClick={handleToggle}
+                  >
+                    <ArrowDropDownIcon />
+                  </Button>
+                </ButtonGroup>
+                <Popper
+                  sx={{
+                    zIndex: 1,
+                  }}
+                  open={open}
+                  anchorEl={anchorRef.current}
+                  role={undefined}
+                  transition
+                  disablePortal
+                >
+                  {({ TransitionProps, placement }) => (
+                    <Grow
+                      {...TransitionProps}
+                      style={{
+                        transformOrigin:
+                          placement === 'bottom' ? 'center top' : 'center bottom',
+                      }}
+                    >
+                      <Paper>
+                        <ClickAwayListener onClickAway={handleClose}>
+                          <MenuList id="split-button-menu" autoFocusItem>
+                            {options.map((option, index) => (
+                              <MenuItem
+                                key={option}
+                                value={option}
+                                selected={index === selectedIndex}
+                                onClick={(event) => handleMenuItemClick(event, index, option)}
+                              >
+                                {option}
+                              </MenuItem>
+                            ))}
+                          </MenuList>
+                        </ClickAwayListener>
+                      </Paper>
+                    </Grow>
+                  )}
+                </Popper>
               </TableCell>
               <TableCell align="right" style={{ fontWeight: '700' }}>
                 Start
@@ -78,7 +178,7 @@ const Upcoming = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {contests.map((item) => (
+            {temp.map((item) => (
               <TableRow
                 key={keyGenerator()}
                 sx={{
@@ -100,20 +200,20 @@ const Upcoming = () => {
                         item.site === 'CodeChef'
                           ? codechefLogo
                           : item.site === 'CodeForces'
-                          ? codeforcesLogo
-                          : item.site === 'AtCoder'
-                          ? atcoderLogo
-                          : item.site === 'TopCoder'
-                          ? topcoderLogo
-                          : item.site === 'HackerRank'
-                          ? hackerrankLogo
-                          : item.site === 'HackerEarth'
-                          ? hackerearthLogo
-                          : item.site === 'LeetCode'
-                          ? leetcodeLogo
-                          : item.site === 'Kick Start'
-                          ? googleLogo
-                          : placeholderLogo
+                            ? codeforcesLogo
+                            : item.site === 'AtCoder'
+                              ? atcoderLogo
+                              : item.site === 'TopCoder'
+                                ? topcoderLogo
+                                : item.site === 'HackerRank'
+                                  ? hackerrankLogo
+                                  : item.site === 'HackerEarth'
+                                    ? hackerearthLogo
+                                    : item.site === 'LeetCode'
+                                      ? leetcodeLogo
+                                      : item.site === 'Kick Start'
+                                        ? googleLogo
+                                        : placeholderLogo
                       }
                       alt=""
                       width={18}
