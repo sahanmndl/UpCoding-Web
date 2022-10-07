f5b825ab5cc361d9566502262642fde7cc945c5f
TanishkM <stanishkmehta5k@gmail.com>
2022-10-07 20:50:00 +0530

    Drak Mode and CSS

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,35 @@
-import React from "react";
+import {React,useState,useEffect} from "react";
 import Header from "../../components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Upcoming from "../Contests/Upcoming";
 import Ongoing from "../Contests/Ongoing";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 const Home = () => {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Upcoming />} />
-                <Route path="/ongoing" element={<Ongoing />} />
-            </Routes>
-        </Router>
-    )
-}
+  const [darkmode, setDarkmode] = useState(false);
+  return (
+    <ThemeProvider theme={darkmode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Router>
+        <Header darkmode={darkmode} setDarkmode={setDarkmode}/>
+        <Routes>
+            <Route path="/" element={<Upcoming darkmode={darkmode} />} />
+            <Route path="/ongoing" element={<Ongoing />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+};
 
-export default Home
+export default Home;
