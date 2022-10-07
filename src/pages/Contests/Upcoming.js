d9ac25bb58238fa6d848cbd3cf52fa99af2afd06
sahanmndl <mondalsahan053@gmail.com>
2022-10-07 18:21:53 +0530

    Fixed Indentation

--- src/pages/Contests/Upcoming.js
+++ src/pages/Contests/Upcoming.js
@@ -22,135 +22,104 @@ import './Contests.css';
 import Colors from '../../utils/Colors';
 
 const Upcoming = () => {
-  const [contests, setContests] = useState([]);
-  const [filterContests, setFilterContests] = useState();
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [refresh, setRefresh] = useState(false);
 
-  const fetchAllContests = async () => {
-    try {
-      const response = await fetch(`https://kontests.net/api/v1/all`);
-      const json = await response.json();
-      setContests([...json]);
-    } catch (e) {
-      console.log(e);
-      setError(e);
-    } finally {
-      setLoading(false);
-      setRefresh(false);
-    }
-  };
+    const [contests, setContests] = useState([]);
+    const [filterContests, setFilterContests] = useState();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [refresh, setRefresh] = useState(false);
 
-  const onRefresh = () => {
-    setRefresh(true);
-    fetchAllContests();
-  };
+    const fetchAllContests = async () => {
+        try {
+            const response = await fetch(`https://kontests.net/api/v1/all`);
+            const json = await response.json();
+            setContests([...json]);
+        } catch (e) {
+            console.log(e);
+            setError(e);
+        } finally {
+            setLoading(false);
+            setRefresh(false);
+        }
+    };
 
-  const keyGenerator = () =>
-    '_' + Math.random().toString(36).substr(2, 9);
+    const onRefresh = () => {
+        setRefresh(true);
+        fetchAllContests();
+    };
 
-  useEffect(() => {
-    fetchAllContests();
-  }, []);
+    const keyGenerator = () =>
+        '_' + Math.random().toString(36).substr(2, 9);
 
-  return (
-    <div className="container">
-      <TableContainer component={Paper} style={{ width: '90%' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ fontWeight: '700' }}>
-                Name
-              </TableCell>
-              <TableCell align="right" style={{ fontWeight: '700' }}>
-                Site
-              </TableCell>
-              <TableCell align="right" style={{ fontWeight: '700' }}>
-                Start
-              </TableCell>
-              <TableCell align="right" style={{ fontWeight: '700' }}>
-                End
-              </TableCell>
-              <TableCell align="right" style={{ fontWeight: '700' }}>
-                Duration
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {contests.map((item) => (
-              <TableRow
-                key={keyGenerator()}
-                sx={{
-                  '&:last-child td, &:last-child th': { border: 0 },
-                }}
-                style={{
-                  backgroundColor:
-                    item.in_24_hours === 'Yes'
-                      ? Colors.GREEN
-                      : 'white',
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  <div
-                    style={{ alignItems: 'center', display: 'flex' }}
-                  >
-                    <img
-                      src={
-                        item.site === 'CodeChef'
-                          ? codechefLogo
-                          : item.site === 'CodeForces'
-                          ? codeforcesLogo
-                          : item.site === 'AtCoder'
-                          ? atcoderLogo
-                          : item.site === 'TopCoder'
-                          ? topcoderLogo
-                          : item.site === 'HackerRank'
-                          ? hackerrankLogo
-                          : item.site === 'HackerEarth'
-                          ? hackerearthLogo
-                          : item.site === 'LeetCode'
-                          ? leetcodeLogo
-                          : item.site === 'Kick Start'
-                          ? googleLogo
-                          : placeholderLogo
-                      }
-                      alt=""
-                      width={18}
-                      height={18}
-                      style={{ marginRight: 8 }}
-                    />
-                    <Link
-                      href={item.url}
-                      style={{ color: Colors.BLUE1 }}
-                    >
-                      {item.name}
-                    </Link>
-                  </div>
-                </TableCell>
-                <TableCell align="right">{item.site}</TableCell>
-                <TableCell align="right">
-                  {new Date(item.start_time).toLocaleString('en-IN', {
-                    timeZone: 'Asia/Kolkata',
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {new Date(item.end_time).toLocaleString('en-IN', {
-                    timeZone: 'Asia/Kolkata',
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {new Date(item.duration * 1000)
-                    .toISOString()
-                    .substr(11, 8)}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+    useEffect(() => {
+        fetchAllContests();
+    }, []);
+
+    return (
+        <div className="container">
+            <TableContainer component={Paper} style={{width: '90%'}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={{fontWeight: '700'}}>Name</TableCell>
+                            <TableCell align="right" style={{fontWeight: '700'}}>Site</TableCell>
+                            <TableCell align="right" style={{fontWeight: '700'}}>Start</TableCell>
+                            <TableCell align="right" style={{fontWeight: '700'}}>End</TableCell>
+                            <TableCell align="right" style={{fontWeight: '700'}}>Duration</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {contests.map((item) => (
+                            <TableRow
+                                key={keyGenerator()}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                style={{backgroundColor: item.in_24_hours === "Yes" ? Colors.GREEN : 'white'}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    <div style={{alignItems: 'center', display: 'flex'}}>
+                                        <img 
+                                            src={item.site === "CodeChef" ? codechefLogo :
+                                                    item.site === "CodeForces" ? codeforcesLogo :
+                                                    item.site === "AtCoder" ? atcoderLogo :
+                                                    item.site === "TopCoder" ? topcoderLogo :
+                                                    item.site === "HackerRank" ? hackerrankLogo :
+                                                    item.site === "HackerEarth" ? hackerearthLogo :
+                                                    item.site === "LeetCode" ? leetcodeLogo :
+                                                    item.site === "Kick Start" ? googleLogo :
+                                                    placeholderLogo
+                                                }
+                                            alt="" 
+                                            width={18} 
+                                            height={18}
+                                            style={{marginRight: 8}}
+                                        />
+                                        <Link
+                                            href={item.url}
+                                            style={{ color: Colors.BLUE1 }}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    </div>
+                                </TableCell>
+                                <TableCell align="right">
+                                    {item.site}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {new Date(item.start_time).toLocaleString("en-IN", {timeZone: 'Asia/Kolkata'})}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {new Date(item.end_time).toLocaleString("en-IN", {timeZone: 'Asia/Kolkata'})}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {new Date(item.duration * 1000).toISOString().substr(11, 8)}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
 };
 
 export default Upcoming;
